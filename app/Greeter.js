fd097d6675a4cfecfd7a2eb1ef64b86f0c93dfc0
shicong <shicongbuct@126.com>
2017-01-11 16:10:13 +0800

    webpack的基本配置

--- app/Greeter.js
+++ app/Greeter.js
@@ -1,7 +1,16 @@
-var config = require('./config.json');
+import React, {Component} from 'react'
+import config from './config.json';
 
-module.exports = function() {
-  var greet = document.createElement('div');
-  greet.textContent = config.greetText;
-  return greet;
-};
+import styles from './Greeter.css';//导入
+
+class Greeter extends Component{
+  render() {
+    return (
+      <div>
+        {config.greetText}
+      </div>
+    );
+  }
+}
+
+export default Greeter;
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
 module.exports = {
   devtool: 'eval-source-map',
   entry:  __dirname + "/app/main.js",//已多次提及的唯一入口文件
   output: {
-    path: __dirname + "/public",//打包后的文件存放的地方
+    path: __dirname + "/build",//打包后的文件存放的地方
     filename: "bundle.js"//打包后输出文件的文件名
   },
   module: {
@@ -15,12 +19,24 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel',//在webpack的module部分的loaders里进行配置即可
-        query: {
-          presets: ['es2015','react']
-        }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css?modules'//添加对样式表的处理
       }
     ]
   },
+  postcss: [
+    require('autoprefixer')//调用autoprefixer插件
+  ],
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + "/app/index.tmpl.html"//new 一个这个插件的实例，并传入相关的参数
+    }),
+    new webpack.HotModuleReplacementPlugin()//热加载插件
+  ],
+
   devServer: {
     contentBase: "./public",//本地服务器所加载的页面所在的目录
     colors: true,//终端中输出结果为彩色
